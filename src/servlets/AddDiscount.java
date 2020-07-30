9afdb2f0aa69d8ce266cbfe0402af33a3609ccce
KhyeLerk <Khye.19@ichat.sp.edu.sg>
2020-07-30 13:43:46 +0800

    Added images

--- src/servlets/AddDiscount.java
+++ src/servlets/AddDiscount.java
@@ -27,7 +27,6 @@ public class AddDiscount extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
